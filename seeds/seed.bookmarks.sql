848b4229ace43f56233843f607a77b35edc53ae0
bix <840573+bix6@users.noreply.github.com>
2020-05-13 14:27:26 -0700

    remove invalid seed param

--- seeds/seed.bookmarks.sql
+++ seeds/seed.bookmarks.sql
@@ -4,4 +4,4 @@ VALUES
     ('Two Two', 'https://hello', 'Greetings', 3),
     ('Three Spot', 'URL', 'What is love', 2),
     ('Quad Quad', 'URL.Com', 'Ahhhhhhh', 0),
-    ('The End', 'website.org', 'The end is near', -2)
+    ('The End', 'website.org', 'The end is near', 1)
